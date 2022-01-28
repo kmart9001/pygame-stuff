1d619bd7b7a07a38959dd126104e3bb3ba457d6e
Kevin Martin <kmart624@gmail.com>
2022-01-28 03:23:27 -0500

    Just adding this project to github

---
+++ pygame/pygametest1.py
@@ -0,0 +1,37 @@
+import pygame
+from sys import exit
+
+pygame.init()
+screen = pygame.display.set_mode((800,400))
+pygame.display.set_caption("Test")
+clock = pygame.time.Clock()
+test_font = pygame.font.Font('dev/pygame/font/Pixeltype.ttf', 50)
+
+sky_surface = pygame.image.load('dev/pygame/graphics/Sky.png').convert()
+ground_surface = pygame.image.load('dev/pygame/graphics/ground.png').convert()
+text_surface = test_font.render('My game', False, 'Black')
+
+snail_surf = pygame.image.load('dev/pygame/graphics/snail/snail1.png').convert_alpha()
+snail_rect = snail_surf.get_rect(bottomright = (600,300))
+
+player_surf = pygame.image.load('dev/pygame/graphics/Player/player_stand.png').convert_alpha()
+player_rect = player_surf.get_rect(midbottom = (80,300))
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
+
+    screen.blit(sky_surface, (0,0))
+    screen.blit(ground_surface, (0,300))
+    screen.blit(text_surface, (300,50))
+    
+    screen.blit(player_surf,player_rect)
+    player_rect.left += 1
+    snail_rect.x -= 4
+    if snail_rect.right <= 0: snail_rect.left = 800
+    screen.blit(snail_surf, snail_rect)
+    
+    pygame.display.update()
+    clock.tick(60)
